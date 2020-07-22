f4504039665ad02047ac454a20ca5e0d6f3ce362
Zaneck <val.owczarek@gmail.com>
2020-07-22 23:33:01 +0200

    doc up

--- src/arithmetic.rs
+++ src/arithmetic.rs
@@ -1,3 +1,9 @@
+/// arithmetic_gcd computes the Greatest Common Divisor of two positive integers.
+///
+/// * `a` - first positive integer
+/// * `b` - second positive integer
+///
+/// the Greatest Common Divisor of 0 and 0 is computed as 0
 pub fn arithmetic_gcd(a: u32, b: u32) -> u32 {
     let mut al = a;
     let mut be = b;
@@ -9,6 +15,10 @@ pub fn arithmetic_gcd(a: u32, b: u32) -> u32 {
     al
 }
 
+/// arithmetic_lcm computes the Least Common Multiple of two positive integers.
+///
+/// * `a` - first positive integer
+/// * `b` - second positive integer
 pub fn arithmetic_lcm(a: u32, b: u32) -> u32 {
     if a & b > 0 {
         (a * b) / arithmetic_gcd(a, b)
